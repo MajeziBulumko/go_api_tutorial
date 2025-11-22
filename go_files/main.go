a94efaeba0a456b3de4a6db96e7d20dbf4771bbb
Bulumko <bulumko@racvert.co.za>
2025-11-22 18:12:23 +0200

    adding mux

--- go_files/main.go
+++ go_files/main.go
@@ -1,35 +1,49 @@
 package main
 
 import (
-	"log"
 	"net/http"
 )
 
-type server struct {
+type api struct {
 	addr string
 }
 
-func (s *server) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	switch r.Method {
-	case http.MethodGet:
-
-		switch r.URL.Path {
-		case "/api/v1/ping":
-			w.Write([]byte("we are here for you"))
-			return
-		case "/api/v2/ping":
-			w.Write([]byte("You are alone"))
-			return
-		default:
-			w.Write([]byte("We need you to stop"))
-			return
-		}
-
-	}
+func (a *api) createUsersHandler(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("User created"))
 }
+func (a *api) getUsersHandler(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("here are all our users"))
+}
+
+// func (s *api) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+// 	switch r.Method {
+// 	case http.MethodGet:
+
+// 		switch r.URL.Path {
+// 		case "/api/v1/ping":
+// 			w.Write([]byte("we are here for you"))
+// 			return
+// 		case "/api/v2/ping":
+// 			w.Write([]byte("You are alone"))
+// 			return
+// 		default:
+// 			w.Write([]byte("We need you to stop"))
+// 			return
+// 		}
+
+// }
 func main() {
-	s := &server{addr: ":8080"}
-	if err := http.ListenAndServe(s.addr, s); err != nil {
-		log.Fatal(err)
+	api := &api{addr: ":8080"}
+
+	mux := http.NewServeMux()
+
+	serve := &http.Server{
+		Addr:    api.addr,
+		Handler: mux,
 	}
+
+	mux.HandleFunc("GET /users", api.getUsersHandler)
+	mux.HandleFunc("POST /users", api.createUsersHandler)
+
+	serve.ListenAndServe()
 }
