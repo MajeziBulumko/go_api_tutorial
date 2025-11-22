352cef27e5306428ebd78b7ccaaa3cbd5589148e
Bulumko <bulumko@racvert.co.za>
2025-11-22 17:27:27 +0200

    starting tut

---
+++ go_files/main.go
@@ -0,0 +1,15 @@
+package main
+
+import "net/http"
+
+type server struct {
+	addr string
+}
+
+func (s *server) ServeHTTP(http.ResponseWriter, *http.Request) {
+
+}
+func main() {
+	s := &server{addr: ":8080"}
+	http.ListenAndServe(s.addr, s)
+}
