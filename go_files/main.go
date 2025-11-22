d66b1dce6658e0f61d20b94675e9f61926feec8e
Bulumko <bulumko@racvert.co.za>
2025-11-22 17:48:46 +0200

    how the http.MethodGet works

--- go_files/main.go
+++ go_files/main.go
@@ -1,15 +1,35 @@
 package main
 
-import "net/http"
+import (
+	"log"
+	"net/http"
+)
 
 type server struct {
 	addr string
 }
 
-func (s *server) ServeHTTP(http.ResponseWriter, *http.Request) {
+func (s *server) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	switch r.Method {
+	case http.MethodGet:
 
+		switch r.URL.Path {
+		case "/api/v1/ping":
+			w.Write([]byte("we are here for you"))
+			return
+		case "/api/v2/ping":
+			w.Write([]byte("You are alone"))
+			return
+		default:
+			w.Write([]byte("We need you to stop"))
+			return
+		}
+
+	}
 }
 func main() {
 	s := &server{addr: ":8080"}
-	http.ListenAndServe(s.addr, s)
+	if err := http.ListenAndServe(s.addr, s); err != nil {
+		log.Fatal(err)
+	}
 }
